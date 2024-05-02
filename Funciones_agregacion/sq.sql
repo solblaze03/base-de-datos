c02dbce26222b23e482753e1391260719676fb80
solblaze03 <maicolbarragan1993@hotmail.com>
2024-05-02 20:29:16 +0200

    Update

--- Funciones_agregacion/sq.sql
+++ Funciones_agregacion/sq.sql
@@ -21,10 +21,10 @@ SELECT Name as "Generos musicales" FROM Genre;
 SELECT Album.Title as Album , Artist.Name as Artista FROM Album join Artist on Album.ArtistId = Artist.ArtistId;
 
 -- Muestra los nombres de los 15 empleados más jóvenes junto a los nombres de sus supervisores, si los tienen.
-SELECT * FROM Employee;
+-- SELECT * FROM Employee;
 SELECT   t2.FirstName as Empleado, t2.BirthDate as menores,
-t2.ReportsTo ,t1.FirstName ,
-t1.EmployeeId as  "Id_supervisor"
+t2.ReportsTo ,t1.FirstName as "Supervisor" ,
+t1.EmployeeId as  "Id_supervisor" 
 FROM Employee as t1 join Employee as t2 
 on t2.ReportsTo = t1.EmployeeId order by t2.BirthDate desc limit 4;
 
@@ -35,14 +35,36 @@ SELECT  I.InvoiceDate,C.FirstName,
  I.Total FROM  Invoice as I join Customer as C on I.CustomerId = C.CustomerId where City like "Berlin";
  
  
+ 
+
+ 
 -- Muestra las listas de reproducción cuyo nombre comienza por C, junto a todas
 -- sus canciones, ordenadas por álbum y por duración
 
-SELECT pl.Name , pl.PlaylistId,plt.PlaylistId as p2, t.TrackId,t.AlbumId, t.Name FROM Playlist as pl join PlaylistTrack as plt 
+SELECT pl.Name , pl.PlaylistId,plt.PlaylistId as p2, t.TrackId,t.AlbumId, t.Name , t.Milliseconds FROM Playlist as pl join PlaylistTrack as plt 
 on pl.PlaylistId = plt.PlaylistId  
 join Track as t 
 on t.TrackId = plt.TrackId 
-where pl.Name like "C%" order by t.AlbumId asc ;
+where pl.Name like "C%" order by t.Milliseconds asc;
+
+-- Muestra qué clientes han realizado compras por valores superiores a 10€, ordenados por apellido.
+SELECT c.LastName, i.Total FROM Customer AS c JOIN Invoice AS i ON c.Customerid = i.Customerid WHERE i.Total > 10 order by c.LastName asc;
+
+-- 
+SELECT * FROM InvoiceLine;
+
+ -- Muestra el número total de artistas.
+SELECT count(*) FROM Artist;
+
+-- Muestra el número de canciones del álbum “Out Of Time”.
+SELECT count(*) FROM Album join Track on Album.AlbumId = Track.AlbumId where Title like "Out of time";
+
+-- Muestra el número de países donde tenemos clientes
+SELECT count(distinct Country) as "Paises Clientes" FROM Customer;
+
+SELECT *,count(Track.Name)  FROM Genre join Track;
+-- Muestra el número de canciones de cada género (deberá mostrarse el nombre del género).
+
 
 
 
